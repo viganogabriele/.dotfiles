4c4d24da184f0609341ee7f7697fdabf17fd7141
Gabriele Viganò <infogabrielevigano@gmail.com>
2026-01-06 22:38:50 +0100

    feat: add uwsm configuration to dotfiles

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # --- Environment Setup ---
 # Use absolute path to ensure the script works regardless of where it's called
 DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-STOW_FOLDERS=("ghostty" "hypr" "swayosd" "walker" "waybar" "xcompose" "zsh" "personal")
+STOW_FOLDERS=("ghostty" "hypr" "swayosd" "walker" "waybar" "xcompose" "zsh" "personal" "uwsm")
 SEP='\n------------------------------------\n'
 
 # Logging helpers
